f707969181553a7502fd261c33bc45ffb783f056
AkramBENHAMMOU <141259806+AkramBENHAMMOU@users.noreply.github.com>
2025-02-24 16:06:36 +0000

    injection dependance  par instanciation dynamique via constructeur

--- src/main/java/presentation/PresentationV2.java
+++ src/main/java/presentation/PresentationV2.java
@@ -1,6 +1,7 @@
 package presentation;
 
 import dao.IDao;
+import metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,10 +12,16 @@ public class PresentationV2 {
     public static void main(String[] args)  {
         try {
             Scanner scanner = new Scanner(new File("config.txt"));
+            //DaoImplV2 d = new DaoImplV2();
             String daoClassName = scanner.nextLine();
             Class cDai = Class.forName(daoClassName);
             IDao o =(IDao) cDai.getConstructor().newInstance();
             System.out.println("Result: "+o.getData());
+            //MetierImpl metier = new MetierImpl(d);
+            String metierClassName = scanner.nextLine();
+            Class cMetier = Class.forName(metierClassName);
+            IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(o);
+            System.out.println("Result: "+metier.calcul());
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } catch (ClassNotFoundException e) {
