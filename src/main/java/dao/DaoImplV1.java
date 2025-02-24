47424a26c6679abdd6211f749d928e7739fd81c6
AkramBENHAMMOU <141259806+AkramBENHAMMOU@users.noreply.github.com>
2025-02-24 17:13:23 +0000

    injection dependance spring annotation

--- src/main/java/dao/DaoImplV1.java
+++ src/main/java/dao/DaoImplV1.java
@@ -1,5 +1,8 @@
 package dao;
 
+import org.springframework.stereotype.Component;
+
+@Component("dao")
 public class DaoImplV1  implements IDao {
     public double getData() {
         System.out.println("Version 1");
--- src/main/java/extension/DaoImplV2.java
+++ src/main/java/extension/DaoImplV2.java
@@ -1,7 +1,9 @@
 package extension;
 
 import dao.IDao;
+import org.springframework.stereotype.Component;
 
+@Component("dao2")
 public class DaoImplV2 implements IDao {
     public double getData() {
         System.out.println("Version 2");
